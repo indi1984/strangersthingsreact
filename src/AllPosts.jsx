d432d2142f060c73279931f729922b53be21b3ee
Kevin Harp <kharp1984@gmail.com>
2023-04-24 12:47:06 -0400

    css updates

--- src/AllPosts.jsx
+++ src/AllPosts.jsx
@@ -30,55 +30,59 @@ const AllPosts = (props) => {
 
   return (
     <Fragment>
-      <br/>
-      <Container fluid>
-        <Row>
+      <Container fluid> 
+        <br/>
+        <Row className="createPostButton">
           <Col>
             {token && !createPost && (
             <Button id="newPostButton"size="lg" variant="warning" type="submit" onClick={()=> setCreatePost(true)}>Create New Post</Button>
             )}
           </Col>
         </Row>
+
         <Row>
-          {token && createPost && (
-          <Fragment>
-            <CreatePost 
-              token={token} 
-              fetchPosts={fetchPosts} 
-              setPostResults={setPostResults} 
-              setCreatePost={setCreatePost}
-            />
-          </Fragment>  
-          )}
+          <Col>
+            {token && createPost && (
+              <CreatePost 
+                token={token} 
+                fetchPosts={fetchPosts} 
+                setPostResults={setPostResults} 
+                setCreatePost={setCreatePost}
+              />
+            )}
+          </Col>
         </Row>
         <br />
+        
         {postResults && postResults.map((post) => {
           return ( 
             <Fragment key={post._id}>
-            <Row>            
-              {post.isAuthor
-              ? <Card bg="light" border="success" style={{ width: '100vh' }}>     
-                  <Card.Body>
-                    <Card.Title>{post.title} <Badge style={{fontSize: 12}} id="username-badge" pill="true" className="mb-3" bg="success" text="light">{post.author.username}</Badge></Card.Title>
-                    <Card.Text>{post.description}</Card.Text>
-                    <Card.Text>{post.price}</Card.Text>
-                    <LinkContainer to="/SinglePost">
-                      <Button variant="success" className="float-end" onClick={() => setPostId(post._id)}>Go to Post</Button>
-                    </LinkContainer>
-                  </Card.Body>   
-                </Card>
-              : <Card bg="light" border="dark" style={{ width: '100vh' }}>
-                  <Card.Body>
-                    <Card.Title>{post.title} <Badge style={{fontSize: 12}} id="username-badge" pill="true" className="mb-3" bg="dark" text="light">{post.author.username}</Badge></Card.Title>
-                    <Card.Text>{post.description}</Card.Text>
-                    <Card.Text>{post.price}</Card.Text>
-                  <LinkContainer to={"/SinglePost"}>
-                    <Button variant="primary" className="float-end" onClick={() => setPostId(post._id)}>Go to Post</Button>
-                  </LinkContainer>  
-                  </Card.Body>
-              </Card>}
-            </Row>
-            <br />
+              <Row>
+                <Col>            
+                  {post.isAuthor
+                  ? <Card bg="light" border="success" style={{ width: '100vh' }}>     
+                      <Card.Body>
+                        <Card.Title>{post.title} <Badge style={{fontSize: 12}} id="username-badge" pill="true" className="mb-3" bg="success" text="light">{post.author.username}</Badge></Card.Title>
+                        <Card.Text>{post.description}</Card.Text>
+                        <Card.Text>{post.price}</Card.Text>
+                        <LinkContainer to="/SinglePost">
+                          <Button variant="success" className="float-end" onClick={() => setPostId(post._id)}>Go to Post</Button>
+                        </LinkContainer>
+                      </Card.Body>   
+                    </Card>
+                  : <Card bg="light" border="dark" style={{ width: '100vh' }}>
+                      <Card.Body>
+                        <Card.Title>{post.title} <Badge style={{fontSize: 12}} id="username-badge" pill="true" className="mb-3" bg="dark" text="light">{post.author.username}</Badge></Card.Title>
+                        <Card.Text>{post.description}</Card.Text>
+                        <Card.Text>{post.price}</Card.Text>
+                      <LinkContainer to={"/SinglePost"}>
+                        <Button variant="primary" className="float-end" onClick={() => setPostId(post._id)}>Go to Post</Button>
+                      </LinkContainer>  
+                      </Card.Body>
+                  </Card>}
+                </Col>
+              </Row>
+              <br />
             </Fragment>
             )
           })
